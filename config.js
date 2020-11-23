b468728d2308ea65f2d5e1d66771caa0df919d7e
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-23 13:41:19 -0700

    pushed stuff

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+    API_ENDPOINT: 'https://boiling-taiga-74199.herokuapp.com/',
+  }
--- src/API/FilmsRater.js
+++ src/API/FilmsRater.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:8000/api/v1/films'
+    baseURL: 'https://stormy-dusk-84915.herokuapp.com/'
 })
