613ad79f8211ea33444ebf4453820ec62bf0dfd8
Dustin Chessnutt <50764104+Dustin1386@users.noreply.github.com>
2020-11-25 13:21:22 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 describe('App', function () {
 
 	it('renders without crashing', () => {
-			const div = document.createElement('div');
-			(<App />);
-			ReactDOM.unmountComponentAtNode(div);
-	});
-});
+			const div = document.createElement('div')
+			(<App />)
+			ReactDOM.unmountComponentAtNode(div)
+	})
+})
